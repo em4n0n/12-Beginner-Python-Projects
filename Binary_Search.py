af6b3bbff35c53a10e851f510cd19c6e2c82afab
em4n0n <58372945+em4n0n@users.noreply.github.com>
2022-05-30 17:36:27 -0700

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,5 +1,6 @@
+import random
+import time
 #[1, 3, 6, 7, 9, 12, 13]
-
 # implement naive search
 def naive_search(l, target):
     for i in range(len(l)):
@@ -14,10 +15,13 @@ def binary_search(l, target, low=None, high=None):
     if high is None:
         high = len(l) - 1
 
+    if high < low:
+        return -1
+
     #getting the midpoint
     midpoint = (low + high)  // 2
 
-    if l [midpoint] == target:
+    if l[midpoint] == target:
         return midpoint
     elif target < l[midpoint]:
         new_high = midpoint - 1
@@ -28,7 +32,27 @@ def binary_search(l, target, low=None, high=None):
         return binary_search(l, target, new_low, high)
 
 if __name__ == '__main__':
-    l = [1, 3, 6, 7, 9, 12, 13]
-    target = 12
-    print(naive_search(l, target))
-    print(binary_search(l, target))
+    # l = [1, 3, 6, 7, 9, 12, 13]
+    # target = 12
+    # print(naive_search(l, target))
+    # print(binary_search(l, target))
+
+    length = 10000
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length))
+    sorted_list = sorted(list(sorted_list))
+
+    target_list = [random.randint(-3*length, 3*length) for _ in range(length)]
+
+    start = time.time()
+    for target in target_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search time:", (end - start), "seconds")
+
+    start = time.time()
+    for target in target_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search time:", (end -start), "seconds")
